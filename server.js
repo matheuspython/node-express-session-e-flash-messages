339459cd7bf1446ac2b2440b7bf0b0e4ef27d57b
matheus souza <ruivorobado@gmail.com>
2021-10-15 18:43:01 -0300

    configurando sessao

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ mongoose.connect(process.env.CONNECTIONSTRING/*isso é uma variavel de ambiente 
 })
 .catch(e => console.log('erro'))
 
+var session = require('express-session');
+var MongoStore = require('connect-mongodb-session')(session);
+
+const flash = require('connect-flash')
+
 
 const routes = require("./routes")
 const path = require('path')
@@ -19,6 +24,18 @@ const {middlewareGlobal} = require("./src/middlewares/middleware")
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.resolve(__dirname, 'public')))
 
+const sessionOptions = session({
+  secret: 'senha',
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,//tempo que a sessao vai durar em milesegundos aqui esta em 7 dias
+    httpOnly: true
+  }
+})
+app.use(sessionOptions)
+app.use(flash())
 
 app.set('views', path.resolve(__dirname, 'src', 'views'))
 app.set('view engine', 'ejs')
